e053b70e267b8a36fde9f93f95a675abcdd79f46
HienPT <yamsunsee@gmail.com>
2023-08-24 16:29:37 +0700

    [Update] Edit Personal Information

--- lib/pages/manage_information.dart
+++ lib/pages/manage_information.dart
@@ -3,9 +3,14 @@ import 'package:ird_connect/configs/index.dart';
 import 'package:ird_connect/providers/index.dart';
 import 'package:provider/provider.dart';
 
-class ManageInformation extends StatelessWidget {
+class ManageInformation extends StatefulWidget {
   const ManageInformation({super.key});
 
+  @override
+  State<ManageInformation> createState() => _ManageInformationState();
+}
+
+class _ManageInformationState extends State<ManageInformation> {
   @override
   Widget build(BuildContext context) {
     return Consumer<UserProvider>(
@@ -17,7 +22,7 @@ class ManageInformation extends StatelessWidget {
             actions: [
               IconButton(
                   onPressed: () {
-                    _edit(context);
+                    _edit(context, user);
                   },
                   icon: const Icon(Icons.edit_note))
             ],
@@ -30,40 +35,36 @@ class ManageInformation extends StatelessWidget {
                   style: StylesConfig.getTextStyleWithColor(context, 'p', 'secondary')),
               const SizedBox(height: 8.0),
               ListTile(
-                leading: const Icon(Icons.screenshot_monitor),
-                title: Text(user.information.displayName),
-                subtitle: const Text('Display Name'),
-                // trailing: const Icon(Icons.more_vert),
-                horizontalTitleGap: -8.0,
-                contentPadding: EdgeInsets.zero,
-                onTap: () {},
-              ),
-              ListTile(
                 leading: const Icon(Icons.perm_contact_cal),
                 title: Text('${user.information.firstName} ${user.information.lastName}'),
                 subtitle: const Text('Full Name'),
                 // trailing: const Icon(Icons.more_vert),
                 horizontalTitleGap: -8.0,
                 contentPadding: EdgeInsets.zero,
-                onTap: () {},
               ),
               ListTile(
-                leading: const Icon(Icons.boy),
+                leading: const Icon(Icons.screenshot_monitor),
+                title: Text(user.information.displayName),
+                subtitle: const Text('Display Name'),
+                // trailing: const Icon(Icons.more_vert),
+                horizontalTitleGap: -8.0,
+                contentPadding: EdgeInsets.zero,
+              ),
+              ListTile(
+                leading: const Icon(Icons.accessibility),
                 title: Text(user.information.gender),
                 subtitle: const Text('Gender'),
                 // trailing: const Icon(Icons.more_vert),
                 horizontalTitleGap: -8.0,
                 contentPadding: EdgeInsets.zero,
-                onTap: () {},
               ),
               ListTile(
-                leading: const Icon(Icons.calendar_month),
+                leading: const Icon(Icons.cake),
                 title: Text(user.information.dateOfBirth),
                 subtitle: const Text('Birthday'),
                 // trailing: const Icon(Icons.more_vert),
                 horizontalTitleGap: -8.0,
                 contentPadding: EdgeInsets.zero,
-                onTap: () {},
               ),
               const SizedBox(height: 16.0),
               Text('Contact', style: StylesConfig.getTextStyle('h6')),
@@ -78,7 +79,6 @@ class ManageInformation extends StatelessWidget {
                 // trailing: const Icon(Icons.more_vert),
                 horizontalTitleGap: -8.0,
                 contentPadding: EdgeInsets.zero,
-                onTap: () {},
               ),
               ListTile(
                 leading: const Icon(Icons.email),
@@ -87,7 +87,6 @@ class ManageInformation extends StatelessWidget {
                 // trailing: const Icon(Icons.more_vert),
                 horizontalTitleGap: -8.0,
                 contentPadding: EdgeInsets.zero,
-                onTap: () {},
               ),
               const SizedBox(height: 16.0),
               Text('Address', style: StylesConfig.getTextStyle('h6')),
@@ -101,7 +100,6 @@ class ManageInformation extends StatelessWidget {
                 // trailing: const Icon(Icons.more_vert),
                 horizontalTitleGap: -8.0,
                 contentPadding: EdgeInsets.zero,
-                onTap: () {},
               ),
               ListTile(
                 leading: const Icon(Icons.work),
@@ -110,7 +108,6 @@ class ManageInformation extends StatelessWidget {
                 // trailing: const Icon(Icons.more_vert),
                 horizontalTitleGap: -8.0,
                 contentPadding: EdgeInsets.zero,
-                onTap: () {},
               ),
             ],
           ),
@@ -119,12 +116,182 @@ class ManageInformation extends StatelessWidget {
     );
   }
 
-  Future<dynamic> _edit(BuildContext context) {
+  Future<dynamic> _edit(BuildContext context, UserProvider user) {
+    final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+    final TextEditingController displayNameController = TextEditingController(text: user.information.displayName);
+    final TextEditingController firstNameController = TextEditingController(text: user.information.firstName);
+    final TextEditingController lastNameController = TextEditingController(text: user.information.lastName);
+    final TextEditingController genderController = TextEditingController(text: user.information.gender);
+    final TextEditingController birthdayController = TextEditingController(text: user.information.dateOfBirth);
+    final TextEditingController phoneNumberController = TextEditingController(text: user.information.phoneNumber);
+    final TextEditingController emailController = TextEditingController(text: user.information.email);
+    final TextEditingController address1Controller = TextEditingController(text: user.information.address1);
+    final TextEditingController address2Controller = TextEditingController(text: user.information.address2);
+
+    void _submit(context) {
+      if (formKey.currentState!.validate()) {
+        final formData = {
+          'displayName': displayNameController.text,
+          'gender': genderController.text,
+        };
+        print(formData);
+        // UserService.login(context, credentials);
+      }
+    }
+
+    String? _validateRequired(String? value, String fieldName) {
+      if (value == null || value.isEmpty) {
+        return 'Please enter your $fieldName.';
+      }
+      return null;
+    }
+
     return showModalBottomSheet(
       context: context,
       builder: (context) {
-        return const Column(
-          children: [Text('Edit information')],
+        return SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 32.0, horizontal: 16.0),
+            child: Form(
+              key: formKey,
+              child: Column(
+                children: [
+                  Text(
+                    'Edit'.toUpperCase(),
+                    style: StylesConfig.getTextStyleWithColor(context, 'h2', 'primary'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Information', style: StylesConfig.getTextStyle('h6')),
+                        Row(children: [
+                          Expanded(
+                            child: TextFormField(
+                              controller: firstNameController,
+                              autovalidateMode: AutovalidateMode.onUserInteraction,
+                              decoration: const InputDecoration(
+                                labelText: 'First Name',
+                                prefixIcon: Icon(Icons.person),
+                              ),
+                              validator: (value) => _validateRequired(value, 'first name'),
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 8.0,
+                          ),
+                          Expanded(
+                            child: TextFormField(
+                              controller: lastNameController,
+                              autovalidateMode: AutovalidateMode.onUserInteraction,
+                              decoration: const InputDecoration(
+                                labelText: 'Last Name',
+                                // prefixIcon: Icon(Icons.person),
+                              ),
+                              validator: (value) => _validateRequired(value, 'last name'),
+                            ),
+                          )
+                        ]),
+                        TextFormField(
+                          controller: displayNameController,
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          decoration: const InputDecoration(
+                            labelText: 'Display Name',
+                            prefixIcon: Icon(Icons.screenshot_monitor),
+                          ),
+                          validator: (value) => _validateRequired(value, 'display name'),
+                        ),
+                        TextFormField(
+                          controller: genderController,
+                          decoration: const InputDecoration(
+                            labelText: 'Gender',
+                            prefixIcon: Icon(Icons.accessibility),
+                          ),
+                        ),
+                        DropdownButtonFormField<String>(
+                          onChanged: (newValue) {
+                            genderController.text = newValue!;
+                          },
+                          items: ['Male', 'Female', 'Other'].map((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                          decoration: const InputDecoration(
+                            labelText: 'Gender',
+                            prefixIcon: Icon(Icons.accessibility),
+                          ),
+                        ),
+                        TextFormField(
+                          readOnly: true,
+                          controller: birthdayController,
+                          decoration: const InputDecoration(
+                            labelText: 'Birthday',
+                            prefixIcon: Icon(Icons.cake),
+                          ),
+                          onTap: () async {
+                            DateTime? selectedDate = await showDatePicker(
+                              context: context,
+                              initialDate: DateTime.now(),
+                              firstDate: DateTime(1900),
+                              lastDate: DateTime.now(),
+                            );
+
+                            if (selectedDate != null) {
+                              birthdayController.text = selectedDate.toString().split(' ')[0];
+                            }
+                          },
+                        ),
+                        const SizedBox(height: 32.0),
+                        Text('Contact', style: StylesConfig.getTextStyle('h6')),
+                        TextFormField(
+                          controller: phoneNumberController,
+                          decoration: const InputDecoration(
+                            labelText: 'Phone Number',
+                            prefixIcon: Icon(Icons.phone),
+                          ),
+                          keyboardType: TextInputType.phone, // Use the phone keyboard
+                        ),
+                        TextFormField(
+                          controller: emailController,
+                          decoration: const InputDecoration(
+                            labelText: 'Email',
+                            prefixIcon: Icon(Icons.email),
+                          ),
+                          keyboardType: TextInputType.emailAddress, // Use the email keyboard
+                        ),
+                        const SizedBox(height: 32.0),
+                        Text('Address', style: StylesConfig.getTextStyle('h6')),
+                        TextFormField(
+                          controller: address1Controller,
+                          decoration: const InputDecoration(
+                            labelText: 'Home',
+                            prefixIcon: Icon(Icons.home),
+                          ),
+                        ),
+                        TextFormField(
+                          controller: address2Controller,
+                          decoration: const InputDecoration(
+                            labelText: 'Office',
+                            prefixIcon: Icon(Icons.work),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      _submit(context);
+                      Navigator.pop(context);
+                    },
+                    child: Text('Submit'.toUpperCase(), style: StylesConfig.getTextStyle('h6')),
+                  ),
+                ],
+              ),
+            ),
+          ),
         );
       },
     );
